fe8cfbc806c07ee55f78158335a637ef6eba36a3
Manav Parmar <manavparmar696@gmail.com>
2024-08-28 21:39:43 +0530

    01 create Heading and ask-question button

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,11 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 function Home() {
   return (
-    <div>
-      <h1 className="h1-bold">Next js13 we&apos;re coming! </h1>
-      <h2 className="h2-bold">Next js13 we&apos;re coming! </h2>
-      <h3 className="h3-bold">Next js13 we&apos;re coming! </h3>
+    <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+      <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+      <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+        <Button className="primary-gradient min-h-[46px] px-4 py-3 text-light-900">
+          Ask a Question
+        </Button>
+      </Link>
     </div>
   );
 }
