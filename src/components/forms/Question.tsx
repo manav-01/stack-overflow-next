1ed286df0921cf00acbd01de3d816df9672a1a2e
Manav Parmar <manavparmar696@gmail.com>
2024-08-29 19:30:11 +0530

    Db connect and create model and use page server concept intend of api

--- src/components/forms/Question.tsx
+++ src/components/forms/Question.tsx
@@ -19,12 +19,12 @@ import { Button } from "../ui/button";
 import { QuestionsSchema } from "@/lib/validations";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
+import { createQuestion } from "@/lib/actions/question.action";
 
 const type: any = "create";
 
 const Question = () => {
   const { mode } = useTheme();
-  console.log(mode);
   const editorRef = useRef(null);
 
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -46,6 +46,7 @@ const Question = () => {
     try {
       // make an async call to your API -> create a question
       // contain all form data
+      await createQuestion();
     } catch (error) {
     } finally {
       setIsSubmitting(false);
---
+++ src/lib/actions/question.action.ts
@@ -0,0 +1,12 @@
+"use server"
+
+import { connectToDatabase } from "../mongoose"
+
+export async function createQuestion() {
+
+    try {
+        connectToDatabase();
+    } catch (error) {
+
+    }
+}
