858cd5d1fe9a8416b2517a28de561fe7790cc402
Manav Parmar <manavparmar696@gmail.com>
2024-08-30 12:20:12 +0530

    create logic of ask-question backend, tags, question, also link tag to question by using server action

--- src/components/forms/Question.tsx
+++ src/components/forms/Question.tsx
@@ -20,12 +20,18 @@ import { QuestionsSchema } from "@/lib/validations";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
 import { createQuestion } from "@/lib/actions/question.action";
+import { useRouter, usePathname } from "next/navigation";
 
 const type: any = "create";
+interface Props {
+  mongoUserId: string;
+}
 
-const Question = () => {
+const Question = ({ mongoUserId }: Props) => {
   const { mode } = useTheme();
   const editorRef = useRef(null);
+  const router = useRouter();
+  const pathname = usePathname();
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
@@ -46,8 +52,18 @@ const Question = () => {
     try {
       // make an async call to your API -> create a question
       // contain all form data
-      await createQuestion();
+      await createQuestion({
+        title: values.title,
+        content: values.explanation,
+        tags: values.tags,
+        author: JSON.parse(mongoUserId),
+        path: pathname,
+      });
+
+      // navigate to home page
+      router.push("/");
     } catch (error) {
+      
     } finally {
       setIsSubmitting(false);
     }
--- src/lib/actions/question.action.ts
+++ src/lib/actions/question.action.ts
@@ -1,12 +1,48 @@
 "use server"
 
+import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose"
+import { CreateQuestionParams } from "./shared.types";
+import Tag from "@/database/tag.model";
+import { revalidatePath } from "next/cache";
 
-export async function createQuestion() {
+
+export async function createQuestion(params: CreateQuestionParams) {
 
     try {
         connectToDatabase();
-    } catch (error) {
+        const { title, content, tags, author, path } = params;
+        // create the question
+        const question = await Question.create({
+            title, content, author
+        })
 
+        const tagDocuments = [];
+
+        // Create the tags or get them if they already exist.
+        for (const tag of tags) {
+            const existingTag = await Tag.findOneAndUpdate(
+                { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+                { $setOnInsert: { name: tag }, $push: { questions: question._id } },
+                { upsert: true, new: true }
+            )
+
+
+            tagDocuments.push(existingTag?._id);
+        }
+
+
+        // add tags id into Question document's tags array element for tag relation.
+        await Question.findOneAndUpdate(question._id, {
+            $push: { tags: { $each: tagDocuments } }
+        });
+
+        // Create am interaction record for the user's ask_question action.
+
+        // Increment author's reputation by +5 for creating a question
+
+        revalidatePath(path);
+    } catch (error) {
+        console.log(error)
     }
 }
