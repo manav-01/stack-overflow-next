708f0b7150b825dd84bc3269072c543fda2a70f7
Manav Parmar <manavparmar696@gmail.com>
2024-09-09 20:04:20 +0530

    applied pagination on Tags and Tags based question page

--- src/lib/actions/tag.actions.ts
+++ src/lib/actions/tag.actions.ts
@@ -38,7 +38,8 @@ export async function getAllTags(params: GetAllTagsParams) {
     try {
         connectToDatabase();
 
-        const { searchQuery, filter } = params;
+        const { searchQuery, filter, page = 1, pageSize = 10 } = params;
+        const skipAmount = (page - 1) * pageSize;
 
         const query: FilterQuery<ITag> = searchQuery ? {
             name: { $regex: new RegExp(searchQuery, "i") }
@@ -66,9 +67,18 @@ export async function getAllTags(params: GetAllTagsParams) {
         }
 
 
-        const tags = await Tag.find(query).sort(sortOptions);
+        const tags = await Tag
+            .find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize);
 
-        return { tags };
+        const totalTags = await Tag.countDocuments(query)
+
+        const isNext = totalTags > skipAmount + tags.length;
+
+
+        return { tags, isNext, totalTags };
 
 
 
@@ -84,6 +94,7 @@ export async function getQuestionByTagId(params: GetQuestionByTagIdParams) {
         connectToDatabase();
 
         const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+        const skipAmount = (page - 1) * pageSize;
 
         const tagFilter: FilterQuery<ITag> = { _id: tagId };
 
@@ -99,6 +110,8 @@ export async function getQuestionByTagId(params: GetQuestionByTagIdParams) {
                 } : {},
                 options: {
                     sort: { createdAt: -1 },
+                    skip: skipAmount,
+                    limit: pageSize + 1 // +1 to check if there is next page
                 },
                 populate: [
                     { path: 'tags', model: Tag, select: "_id name" },
@@ -113,8 +126,10 @@ export async function getQuestionByTagId(params: GetQuestionByTagIdParams) {
             throw new Error('Tag not found');
         }
 
+        const isNext = tag.questions.length > pageSize;
+
         const questions = tag.questions;
-        return { tagTitle: tag.name, questions }
+        return { tagTitle: tag.name, questions, isNext, totalTags: tag.questions?.length || 0 }
 
 
     } catch (error) {
