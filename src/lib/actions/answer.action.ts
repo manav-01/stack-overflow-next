6c815893485cf10d638998f19ab693e47a2ff071
Manav Parmar <manavparmar696@gmail.com>
2024-09-09 22:49:28 +0530

    applied pagination inside question's all Answer page

--- src/lib/actions/answer.action.ts
+++ src/lib/actions/answer.action.ts
@@ -35,7 +35,9 @@ export async function getAnswers(params: GetAnswersParams) {
     try {
         connectToDatabase();
 
-        const { questionId, sortBy } = params;
+        const { questionId, sortBy, page = 1, pageSize = 10 } = params;
+
+        const skipAmount = (page - 1) * pageSize;
 
         let sortOptions = {};
 
@@ -61,8 +63,15 @@ export async function getAnswers(params: GetAnswersParams) {
             { question: questionId })
             .populate("author", "_id clerkId name picture")
             .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize);
 
-        return { answers };
+
+        const totalAnswer = await Answer.countDocuments({ question: questionId });
+
+        const isNextAnswer = totalAnswer > skipAmount + answers.length;
+
+        return { answers, isNextAnswer, totalAnswer };
     } catch (error) {
         console.log(error);
         throw error;
