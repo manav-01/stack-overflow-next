1a29d9d44b0cbdbf1939895060cba45a0124006c
Manav Parmar <manavparmar696@gmail.com>
2024-09-24 21:24:38 +0530

    fixed bug

--- src/app/(root)/profile/[id]/page.tsx
+++ src/app/(root)/profile/[id]/page.tsx
@@ -96,14 +96,20 @@ async function Page({ params, searchParams }: URLProps) {
               Answers
             </TabsTrigger>
           </TabsList>
-          <TabsContent value="top-posts" className="flex w-full flex-col gap-6">
+          <TabsContent
+            value="top-posts"
+            className="mt-5 flex w-full flex-col gap-6"
+          >
             <QuestionTab
               searchParams={searchParams}
               userId={userInfo.user._id}
               clerkId={clerkId}
             />
           </TabsContent>
-          <TabsContent value="answers" className="flex w-full flex-col gap-6">
+          <TabsContent
+            value="answers"
+            className="mt-5 flex w-full flex-col gap-6"
+          >
             <AnswersTab
               searchParams={searchParams}
               userId={userInfo.user._id}
